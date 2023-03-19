064e35bc513172de41d2e4834d7a692ce4f057c7
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2023-03-18 22:24:43 -0500

    build country services and show result of request

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { CountryModel } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
 
+  termino: string = '';
+  hayError: boolean = false;
+  paises: CountryModel[] = [];
+
+  constructor(private paisService: PaisService) {}
+
+  buscar() {
+    this.hayError = false;
+
+    this.paisService.buscarPais(this.termino)
+      .pipe(
+        catchError((err) => {
+          this.hayError = true;
+          this.paises = [];
+          return of([]);
+        })
+      )
+      .subscribe((resp: CountryModel[]) => {  
+        this.paises = resp
+        console.log(resp);
+        
+      });
+  }
 }
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CountryModel } from '../interfaces/pais.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor( private http: HttpClient ) { };
+
+  buscarPais(termino: string): Observable<CountryModel[]> {
+    const url = `${this.apiUrl}/name/${termino}`
+
+    return this.http.get<CountryModel[]>(url);
+  }
+
+}
